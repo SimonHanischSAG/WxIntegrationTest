169a053534ad17a83cae2f167da7b412c7805d17
Simon Hanisch <simon.hanisch@softwareag.com>
2022-07-22 10:56:06 +0200

    Refactorings: Added config project, renamced queues, topics and triggers

---
+++ packages/WxIntegrationTestControllerConfig/code/source/wx/integrationTest/controller/impl/testSuite.java
@@ -0,0 +1,167 @@
+package wx.integrationTest.controller.impl;
+
+// -----( IS Java Code Template v1.2
+
+import com.wm.data.*;
+import com.wm.util.Values;
+import com.wm.app.b2b.server.Service;
+import com.wm.app.b2b.server.ServiceException;
+// --- <<IS-START-IMPORTS>> ---
+import com.wm.app.b2b.server.Server;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+import com.softwareag.de.s.gitjendis.builder.utils.TestSuiteRunner;
+import com.softwareag.util.IDataMap;
+// --- <<IS-END-IMPORTS>> ---
+
+public final class testSuite
+
+{
+	// ---( internal utility methods )---
+
+	final static testSuite _instance = new testSuite();
+
+	static testSuite _newInstance() { return new testSuite(); }
+
+	static testSuite _cast(Object o) { return (testSuite)o; }
+
+	// ---( server methods )---
+
+
+
+
+	public static final void executeTestSuite (IData pipeline)
+        throws ServiceException
+	{
+		// --- <<IS-START(executeTestSuite)>> ---
+		// @sigtype java 3.5
+		// [i] field:0:required package
+		// [i] field:0:required isUrl
+		// [i] field:0:required isUser
+		// [i] field:0:required isPassword
+		// [i] field:0:required coverage {"false","true"}
+		// [i] field:0:required testSuiteFileFilter
+		// [o] field:0:required reportFile
+		final IDataMap map = new IDataMap(pipeline);
+		final Function<String,String> paramGetter = (p) -> {
+			final String v = map.getAsString(p);
+			if (v == null) {
+				throw new NullPointerException("Missing parameter: " + p);
+			}
+			if (v.length() == 0) {
+				throw new IllegalArgumentException("Empty parameter: " + p);
+			}
+			return v;
+		};
+		final String packageName = paramGetter.apply("package");
+		final Path packageDir = Paths.get("./packages/" + packageName);
+		final Predicate<String> testSuiteFileFilter = asFilter(map.getAsString("testSuiteFileFilter"));
+		if (!Files.isDirectory(packageDir)) {
+			throw new IllegalArgumentException("Invalid value for parameter package: "
+					+ packageName + " (Package directory not found: " + packageDir + ")");
+		}
+		final String isUrlStr = paramGetter.apply("isUrl");
+		final URL isUrl;
+		try {
+			isUrl = new URL(isUrlStr);
+		} catch (MalformedURLException e) {
+			throw new IllegalArgumentException("Invalid value for parameter isUrl: "
+					+ " Expected valid URL, got " + isUrlStr);
+		}
+		final String isUser = paramGetter.apply("isUser");
+		final String isPassword = paramGetter.apply("isPassword");
+		final boolean coverage = map.getAsBoolean("coverage", Boolean.FALSE).booleanValue();
+		final Path tempDir = Paths.get("./temp/packages/tests");
+		try {
+			Files.createDirectories(tempDir);
+			final Path testDir = Files.createTempDirectory(tempDir, "ts");
+			final Path instanceDir = Server.getHomeDir().toPath();
+			final Path instancesDir = instanceDir.getParent();
+			final Path isDir = instancesDir.getParent();
+			final Path wmHomeDir = isDir.getParent();
+			final Path reportFile = new TestSuiteRunner(wmHomeDir).run(packageDir, (s) -> true, testSuiteFileFilter,
+					                                                   testDir,
+												                       isUrl, isUser, isPassword, coverage);
+			if (reportFile != null) {
+				map.put("reportFile", reportFile.toString());
+			}
+		} catch (IOException e) {
+			throw new ServiceException(e);
+		}
+			
+		// --- <<IS-END>> ---
+
+                
+	}
+
+	// --- <<IS-START-SHARED>> ---
+	
+	private static Predicate<String> asFilter(String pFilterStr) {
+		if (pFilterStr == null  ||  pFilterStr.length() == 0) {
+			return (s) -> true;
+		} else {
+			String filterStr = pFilterStr;
+			final String patternStr;
+			boolean caseInsensitive;
+			if (filterStr.endsWith("/i")) {
+				caseInsensitive = true;
+				filterStr = filterStr.substring(0, filterStr.length()-2);
+			} else {
+				caseInsensitive = false;
+			}
+			if (filterStr.startsWith("re:")) {
+				patternStr = filterStr.substring(3);
+			} else {
+				final StringBuilder sb = new StringBuilder();
+				sb.append("^");
+				for (int i = 0;  i < filterStr.length();  i++) {
+					final char c = filterStr.charAt(i);
+					switch (c) {
+					  case '?':
+						sb.append(".");
+						break;
+					  case '*':
+						sb.append(".*");
+						break;
+					  case '\\':
+					  case '[':
+					  case ']':
+					    sb.append("\\");
+					    sb.append(c);
+					    break;
+					  default:
+						sb.append(c);
+						break;
+					}
+				}
+				sb.append("$");
+				patternStr = sb.toString();
+			}
+			final Pattern pattern;
+			try {
+				if (caseInsensitive) {
+					pattern = Pattern.compile(patternStr, Pattern.CASE_INSENSITIVE);
+				} else {
+					pattern = Pattern.compile(patternStr);
+				}
+			} catch (PatternSyntaxException pse) {
+				throw new IllegalArgumentException("Invalid filter string " + pFilterStr +
+						                           " (converted to pattern " + patternStr);
+			}
+			return (s) -> {
+				return pattern.matcher(s).matches();
+			};
+		}
+	}
+		
+	// --- <<IS-END-SHARED>> ---
+}
+
---
+++ packages/WxIntegrationTestControllerConfig/code/source/wx/integrationTest/controller/impl/util.java
@@ -0,0 +1,292 @@
+package wx.integrationTest.controller.impl;
+
+// -----( IS Java Code Template v1.2
+
+import com.wm.data.*;
+import com.wm.util.Values;
+import com.wm.app.b2b.server.Service;
+import com.wm.app.b2b.server.ServiceException;
+
+import java.io.IOException;
+// --- <<IS-START-IMPORTS>> ---
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Date;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import com.softwareag.util.IDataMap;
+// --- <<IS-END-IMPORTS>> ---
+
+public final class util
+
+{
+	// ---( internal utility methods )---
+
+	final static util _instance = new util();
+
+	static util _newInstance() { return new util(); }
+
+	static util _cast(Object o) { return (util)o; }
+
+	// ---( server methods )---
+
+
+
+
+	public static final void dropPipelineVariable (IData pipeline)
+        throws ServiceException
+	{
+		// --- <<IS-START(dropPipelineVariable)>> ---
+		// @sigtype java 3.5
+		// [i] field:0:required variableName
+		IDataMap pMap = new IDataMap(pipeline);
+		String variableName = pMap.getAsString("variableName");
+		pMap.remove(variableName);
+		
+		// --- <<IS-END>> ---
+
+                
+	}
+
+
+
+	public static final void notifyPollingService (IData pipeline)
+        throws ServiceException
+	{
+		// --- <<IS-START(notifyPollingService)>> ---
+		// @sigtype java 3.5
+		// [i] field:0:required id
+		// [i] record:0:required pipeline
+		IDataMap pipeMap = new IDataMap(pipeline);
+		String id = pipeMap.getAsString("id");
+		if (id == null) {
+			throw new ServiceException("notifyPollingService: id is empty");
+		}
+		IData genericResponse = pipeMap.getAsIData("genericResponse");
+		// log("SITC: " + id);
+		// log("SITC: length of arrivedResponses before notifying:" +
+		// arrivedResponses.size());
+		
+		cleanUpArrivedResponses();
+		
+		// log("SITC: before put :" + id + "," + arrivedResponses);
+		
+		Response alreadyArrivedResponse = arrivedResponses.get(id);
+		if (alreadyArrivedResponse == null) {
+			Response response = new Response();
+			response.date = new Date();
+			response.genericResponse = genericResponse;
+		
+			arrivedResponses.put(id, response);
+		} else {
+			alreadyArrivedResponse.date = new Date();
+			alreadyArrivedResponse.genericResponse = genericResponse;
+			alreadyArrivedResponse.responseCount++;
+			arrivedResponses.put(id, alreadyArrivedResponse);
+		}
+		
+		// log("SITC: length of arrivedResponses after notifying:" +
+		// arrivedResponses.size());
+		
+			
+		// --- <<IS-END>> ---
+
+                
+	}
+
+
+
+	public static final void pollForMessage (IData pipeline)
+        throws ServiceException
+	{
+		// --- <<IS-START(pollForMessage)>> ---
+		// @sigtype java 3.5
+		// [i] field:0:required id
+		// [i] field:0:optional waitForResponseFromXClients
+		// [o] record:0:optional pipeline
+		// log("SITC: length of arrivedResponses before polling:" +
+		// arrivedResponses.size());
+		
+		IDataMap pipeMap = new IDataMap(pipeline);
+		String id = pipeMap.getAsString("id");
+		if (id == null) {
+			throw new ServiceException("pollForMessage: id is empty");
+		}
+		
+		String timeoutInSeconds = pipeMap.getAsString("timeoutInSeconds");
+		int myPollingCountMax = pollingCountMax;
+		if (timeoutInSeconds != null && !timeoutInSeconds.equals("")) {
+			myPollingCountMax = Integer.valueOf(timeoutInSeconds);
+		}
+		
+		String waitForResponseFromXClients = pipeMap.getAsString("waitForResponseFromXClients");
+		int waitForResponseFromXClientsInt = 1;
+		if (waitForResponseFromXClients != null && !waitForResponseFromXClients.equals("")) {
+			waitForResponseFromXClientsInt = Integer.valueOf(waitForResponseFromXClients);
+		}
+		
+		
+		
+		Date foundAndRemovedValue = null;
+		int foundResponseCount = 0;
+		int pollingCount = 0;
+		do {
+			try {
+				Thread.sleep(pollingInterval);
+			} catch (InterruptedException e) {}
+			//log("SITC: length of arrivedResponses during polling:" + arrivedResponses.size());
+			//log("SITC: check for " + id + " in arrivedResponses");
+			Response response = arrivedResponses.remove(id);
+			//log("SITC: response " + response);
+			if (response != null) {
+				//log("SITC: response not null");
+				foundResponseCount = response.responseCount;
+				//log("foundResponseCount: " + foundResponseCount);
+				foundAndRemovedValue = response.date;
+				if (response.genericResponse != null) {
+					pipeMap.put("genericResponse", response.genericResponse);
+				}
+			}
+			pollingCount++;
+		} while (foundResponseCount < waitForResponseFromXClientsInt && pollingCount < myPollingCountMax);
+		
+		if (foundAndRemovedValue == null) {
+			throw new ServiceException("pollForMessage: Timeout during polling for response for id: " + id);
+		}
+		// log("SITC: length of arrivedResponses after polling:" +
+		// arrivedResponses.size());
+		
+			
+			
+		// --- <<IS-END>> ---
+
+                
+	}
+
+
+
+	public static final void readFile (IData pipeline)
+        throws ServiceException
+	{
+		// --- <<IS-START(readFile)>> ---
+		// @sigtype java 3.5
+		// [i] field:0:required file
+		// [o] object:0:required bytes
+		final IDataMap map = new IDataMap(pipeline);
+		final String fileStr = map.getAsString("file");
+		if (fileStr == null) {
+			throw new NullPointerException("Missing parameter: file");
+		}
+		if (fileStr.length() == 0) {
+			throw new IllegalArgumentException("Empty parameter: file");
+		}
+		final Path file = Paths.get(fileStr);
+		if (!Files.isRegularFile(file)) {
+			throw new IllegalArgumentException("Illegal argument for parameter file: " + file + " (Doesn't exist, or is not a file)");
+		}
+		try {
+			map.put("bytes", Files.readAllBytes(file));
+		} catch (IOException e) {
+			throw new ServiceException(e);
+		}		
+		// --- <<IS-END>> ---
+
+                
+	}
+
+
+
+	public static final void replacePipeline (IData pipeline)
+        throws ServiceException
+	{
+		// --- <<IS-START(replacePipeline)>> ---
+		// @sigtype java 3.5
+		// [i] record:0:required compensatingPipeline
+		IDataMap pipeMap = new IDataMap(pipeline);
+		IData compensatingPipeline = pipeMap.getAsIData("compensatingPipeline");
+		
+		IDataMap compensatingPipeMap = new IDataMap(compensatingPipeline);
+		
+		pipeMap.clear();
+		
+		Set<String> keys = compensatingPipeMap.keySet();
+		for (String key : keys) {
+			pipeMap.put(key, compensatingPipeMap.get(key));
+		}
+		
+		// --- <<IS-END>> ---
+
+                
+	}
+
+
+
+	public static final void resolvePath (IData pipeline)
+        throws ServiceException
+	{
+		// --- <<IS-START(resolvePath)>> ---
+		// @sigtype java 3.5
+		// [i] field:0:required directory
+		// [i] field:0:required relativePath
+		// [o] field:0:required outputPath
+		final IDataMap map = new IDataMap(pipeline);
+		final String dirStr = map.getAsString("directory");
+		if (dirStr == null) {
+			throw new NullPointerException("Missing parameter: directory");
+		}
+		final Path dir = Paths.get(dirStr);
+		final String relativePath = map.getAsString("relativePath");
+		if (relativePath == null) {
+			throw new NullPointerException("Missing parameter: relativePath");
+		}
+		map.put("outputPath", dir.resolve(relativePath).toString());
+		// --- <<IS-END>> ---
+
+                
+	}
+
+	// --- <<IS-START-SHARED>> ---
+	
+	private static long pollingInterval = 1000;
+	private static int pollingCountMax = 20;
+	private static ConcurrentHashMap<String, Response> arrivedResponses = new ConcurrentHashMap<String, Response>();
+	
+	private static void cleanUpArrivedResponses() {
+		// log("SITC: start cleanup");
+		long currentDate = new Date().getTime();
+		Set<Entry<String, Response>> set = arrivedResponses.entrySet();
+		for (Entry<String, Response> entry : set) {
+			if ((currentDate - entry.getValue().date.getTime()) > 2 * pollingInterval) {
+				arrivedResponses.remove(entry.getKey());
+				// log("SITC: removed");
+			}
+		}
+		// log("SITC: end cleanup");
+	}
+	
+	private static class Response {
+	
+		private Date date;
+		private IData genericResponse;
+		private int responseCount = 1;
+	}
+	
+	public static void log(String message) {
+		// input
+		IData input = IDataFactory.create();
+		IDataCursor inputCursor = input.getCursor();
+		IDataUtil.put(inputCursor, "message", message);
+		inputCursor.destroy();
+	
+		try {
+			Service.doInvoke("pub.flow", "debugLog", input);
+		} catch (Exception e) {
+		}
+	}
+	
+		
+	// --- <<IS-END-SHARED>> ---
+}
+
